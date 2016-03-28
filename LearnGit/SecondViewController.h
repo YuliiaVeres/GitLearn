7bf97cd8bbba7e2af2070cd11923082817b518d2
Yuliia Veres <juliaveresklia@yahoo.com>
2016-03-28 22:07:29 +0300

    Configured ViewController, added SecondViewController

---
+++ LearnGit/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  LearnGit
+//
+//  Created by Yuliia Veresklia on 3/28/16.
+//  Copyright © 2016 Yuliia Veresklia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
