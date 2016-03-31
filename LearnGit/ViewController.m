72b005e264303b6ba2ca7e392d896db019a0dc5e
Yuliia Veres <juliaveresklia@yahoo.com>
2016-03-31 10:31:38 +0300

    Tried forst test task

--- LearnGit/ViewController.m
+++ LearnGit/ViewController.m
@@ -17,6 +17,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSArray *first = @[@(1), @(2), @(3)];
+    NSArray *second = @[first, @(4)];
+    NSArray *third = @[@(6), @(7)];
+    
+    NSArray *arrayToTest = @[second, @(5), third, @(8)];
+    
+    NSArray *result = [self processArray:arrayToTest];
+    
+    NSLog(@"Result array: %@", result);
+}
+
+#pragma mark - Test task attempt
+
+- (NSArray *)processArray:(NSArray *)elementsArray
+{
+    NSMutableArray *resultArray = [NSMutableArray new];
+    
+    for (id element in elementsArray)
+    {
+        if ([element isKindOfClass:[NSArray class]])
+        {
+            NSArray *arrayResults = [self processArray:element];
+            [resultArray addObjectsFromArray:arrayResults];
+        }
+        else
+            [resultArray addObject:element];
+    }
+    
+    return resultArray.copy;
 }
 
 @end
